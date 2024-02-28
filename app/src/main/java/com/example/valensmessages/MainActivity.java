009cf246b5206c7d475b6d9f6a961141dfac094c
ValenMR <valentinamunoz1010@gmail.com>
2024-02-27 23:25:55 -0500

    Creando funcion "buttonPress" para la funcionalidad del botón

--- app/src/main/java/com/example/valensmessages/MainActivity.java
+++ app/src/main/java/com/example/valensmessages/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.valensmessages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonPress(View view){
+
+        Log.i("Info", "Botón presionado");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
