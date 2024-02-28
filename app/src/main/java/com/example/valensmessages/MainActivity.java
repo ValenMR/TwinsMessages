a02a1afea121f463fce75bc0b2b3def80eb9b12f
ValentinaC <valentina.cadenaz@udea.edu.co>
2024-02-28 15:12:11 -0500

    feat: Crear socket e hilo asincrono

--- app/src/main/java/com/example/valensmessages/MainActivity.java
+++ app/src/main/java/com/example/valensmessages/MainActivity.java
@@ -9,27 +9,41 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static MainActivity instance;
 
     TextView mensajeTextView; /*variable de instancia del tipo TextView*/
     EditText mensajeEditText;  /*variable de instancia*/
 
+    NetworkManager nm;
+
     public void buttonPress(View view){
 
         Log.i("Info", "Botón presionado");
-
         mensajeEditText = findViewById(R.id.mensajeEditText);  /*ya se estaría poniendo en EditText, cuando presione el botón es que se edita*/
-
         String mensajeString = mensajeEditText.getText().toString();  /*convertir lo de editText en string*/
-        mensajeTextView.setText(mensajeString);
+        nm.enviarmensaje(mensajeString);
 
+    }
+
+    public void agregarTexto (String textoAAgregar){
+        mensajeEditText = findViewById(R.id.mensajeEditText);  /*ya se estaría poniendo en EditText, cuando presione el botón es que se edita*/
+        String actual = this.mensajeEditText.getText().toString();
+        actual.concat(textoAAgregar);
+        mensajeTextView.setText(actual);
+    }
+
+    public static MainActivity getInstance() {
+        return instance;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        instance = this;
         super.onCreate(savedInstanceState);   /*método de OnCreate*/
         setContentView(R.layout.activity_main);  /*asigna el contenido de la vista*/
         mensajeTextView = findViewById(R.id.mensajeTextView); /* creando la conexión al botón -  a esta var de instancia va a hacer una conexión con el elemento textView */
         mensajeTextView.setText("Hola Mundo"); /*utilizando la variable - se le está pasando como parámetro el mensaje hola mundo*/
-        new NetworkManager().execute();
+        nm = new NetworkManager(this);
+        nm.execute();
     }
 }
--- app/src/main/java/com/example/valensmessages/NetworkManager.java
+++ app/src/main/java/com/example/valensmessages/NetworkManager.java
@@ -1,29 +1,102 @@
 package com.example.valensmessages;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
-
+import android.text.format.Formatter;
+import android.util.Log;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class NetworkManager extends AsyncTask<Void, Void, Void> {
+    private Activity activity;
+    private Context context; // Agregar esta línea
 
     Socket socket; // Reemplaza "192.168.0.1" con la dirección IP del servidor y "12345" con el puerto del servidor
     DataOutputStream outputStream;
     DataInputStream inputStream;
 
+    public NetworkManager(Activity activity) {
+        this.activity = activity;
+        this.context = activity.getApplicationContext(); // Agregar esta línea
+    }
+
     @Override
     protected Void doInBackground(Void... voids) {
         try {
+
+            WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int ipAddress = wifiInfo.getIpAddress();
+            String ip = Formatter.formatIpAddress(ipAddress);
+            Log.d("ip", "ip: " + ip);
+
             // Realiza la conexión y obtiene los streams de entrada y salida
-            socket = new Socket("192.168.0.1", 12345);
+            socket = new Socket("192.168.20.120", 1234); //puerto no bloqueado, ip del celular
             outputStream = new DataOutputStream(socket.getOutputStream());
             inputStream = new DataInputStream(socket.getInputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            final String message = inputStream.readUTF();
+                            // Muestra el mensaje en la interfaz de usuario
+                            activity.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    MainActivity.getInstance().agregarTexto("asd");
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+
         } catch (IOException e) {
-            // Maneja cualquier excepción que se produzca durante la conexión
+            Log.i("ERR", "error de io");
             e.printStackTrace();
         }
         return null;
     }
+
+    public Void enviarmensaje(String valor){
+
+        try {
+            Log.i("info", "valor de outputstream" + outputStream );
+
+            outputStream.writeUTF(valor);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    Log.e("NetworkManager", "Error closing output stream", e);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        if (outputStream != null) {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
